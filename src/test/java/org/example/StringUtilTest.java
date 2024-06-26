914e7f67ebc080f323b5b62866e289cf8aac1179
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-06-25 23:26:02 -0700

    using JUnit

--- src/test/java/org/example/StringUtilTest.java
+++ src/test/java/org/example/StringUtilTest.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import org.junit.Assert;
+import org.junit.Test;
 
-class StringUtilTest {
-    public static void main(String[] args) {
+public class StringUtilTest {
+
+    @Test
+    public  void testRepeat() {
         Assert.assertEquals(StringUtil.repeat("Hello World", 3), "Hello WorldHello WorldHello World");
         Assert.assertEquals(StringUtil.repeat("Hello World", 1), "Hello World");
     }
