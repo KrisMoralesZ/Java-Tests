52950b7ae13658c70246f425530f78b8642974b4
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-06-25 23:22:45 -0700

    using JUnit

--- src/test/java/org/example/StringUtilTest.java
+++ src/test/java/org/example/StringUtilTest.java
@@ -1,14 +1,10 @@
 package org.example;
 
+import org.junit.Assert;
+
 class StringUtilTest {
     public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("Hello World", 3), "Hello WorldHello WorldHello World");
-        assertEquals(StringUtil.repeat("Hello World", 1), "Hello World");
-    }
-
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equal to expected" + expected);
-        }
+        Assert.assertEquals(StringUtil.repeat("Hello World", 3), "Hello WorldHello WorldHello World");
+        Assert.assertEquals(StringUtil.repeat("Hello World", 1), "Hello World");
     }
 }
